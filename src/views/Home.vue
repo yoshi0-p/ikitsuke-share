da9e3582f890729c43386fa027e2349d36027301
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-15 17:10:18 +0900

    ss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,19 @@
       
       <div class="parent">
       <div class="logo">
-        <h2 >イキツケ</h2>
-        <h1 >SHARE</h1>
+        <h2 ><span class="red">イ</span>キツケ<span class="blue">S</span>HARE</h2>
       </div>
-      <HeaderMenu />
-      <Serch ></Serch>
+      <p class="catch">誰かのイキツケはきっといい店だ。</p>
+      <div class="post">
+        <p>オススメのお店を投稿する</p>
+        <HeaderMenu />
+      </div>
+      <div class="serch">
+        <p>自分の街のオススメ店を見る</p>
+        <Serch ></Serch>
+        <Item ></Item>
+      </div>
+      
       <Item ></Item>
       </div>
 </template>
@@ -28,7 +36,27 @@ export default {
 
 </script>
 <style scoped>
-
+.logo{
+  color: white;
+  background-color: black;
+  height:50px;
+  line-height: 50px;
+}
+.post{
+  background-color: rgb(224, 141, 141);
+  color:white;
+}
+.serch{
+  background-color: rgb(141, 187, 224);
+  color:white;
+}
+.red{
+  color:red;
+}
+.blue{
+  color:blue;
+}
 </style>
+
 
 
