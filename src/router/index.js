e518d7bf86215973c6c0e7a2d201715e306b803f
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-21 20:00:50 +0900

    d

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 import Login from "../views/Login.vue";
 import SignUp from "../views/SignUp.vue";
 
@@ -11,6 +12,11 @@ const routes = [
     path: "/login",
     name: "login",
     component: Login,
+  },
+  {
+    path: "/about",
+    name: "about",
+    component: About,
   },
  {
     path: "/signup",
--- src/views/About.vue
+++ src/views/About.vue
@@ -7,7 +7,6 @@
 import Header from "../components/Header";
 export default {
   components: {
-    
     Header
   }
 };
