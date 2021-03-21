09da8b0dde4886443f5aecf35efc7be2c2979579
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-21 19:57:48 +0900

    d

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+   <Header />
   </div>
 </template>
+<script>
+import Header from "../components/Header";
+export default {
+  components: {
+    
+    Header
+  }
+};
+</script>
