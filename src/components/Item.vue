62f4532b2e8d022a5ab6c890efdd96c45cd2dd33
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-26 23:07:56 +0900

    dd

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <div class="pre" v-if= "$store.state.wanted_pref_id && $store.state.wanted_wanted_type">
+  <div class="pre" >
       <p>{{$store.state.wanted_pref_id}}の「{{$store.state.wanted_wanted_type}}」イキツケ一覧
       </p>
    </div>
