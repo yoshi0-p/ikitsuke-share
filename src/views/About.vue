d6498c30efbdb2735c1d098e0445413c56b4ad1f
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-26 23:40:33 +0900

    dd

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="about">
    <Header />
+   <h3>使い方は「紹介する」と「探す」の２つ！</h3>
    <img src="../assets/howto.jpg" alt="">
+   <h3>Q＆A</h3>
+   <p class="q">一回しか行ったことがない店でも投稿して良いですか？</p>
+   <p class="a">はい。ぜひシェアしてください。これからも行きたいと思えた良店を皆で共有するアプリとしてご活用ください！</p>
+   <p class="q">自分の店を投稿しても良いですか？</p>
+   <p class="a">はい。ぜひシェアしてください。但し明らかに同じ人が宣伝として何度も、あるいはシーズンごとキャンペーンごとに投稿し情報の質を下げていると判断した場合は投稿を削除させていただく場合があります。</p>
+   <p class="q">イキツケの一覧が表示されないのですが？</p>
+   <p class="a">申し訳ありませんが、そのエリアではまだ誰もイキツケをシェアされていないようです。ぜひオススメのお店を教えてください！</p>
   </div>
 </template>
 <script>
@@ -15,5 +23,13 @@ export default {
 <style scoped>
 img{
   width:100%;
+}
+.q{
+  color:black;
+  padding-top:10px;
+}
+.a{
+  color:rgb(61, 61, 61);
+  padding-bottom:10px;
 }
 </style>
