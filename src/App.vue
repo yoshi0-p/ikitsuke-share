958e4a4c9dca3223691782534f94292675e857ec
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-27 10:00:21 +0900

    dd

--- src/App.vue
+++ src/App.vue
@@ -115,35 +115,4 @@ h1{
 }
 </style>
 <script>
-
-export default {
-    methods : {
-  	createPageTitle : function(to){
-		 // タイトルを設定
-		if(to.meta.title){
-			var setTitle = to.meta.title;
-			document.title = setTitle;
-		} else {
-			document.title = 'ikituke-share'
-		}
-
-		// メタタグdescription設定
-		if(to.meta.desc){
-			var setDesc = to.meta.desc;
-			document.querySelector("meta[name='description']").setAttribute('content', setDesc)
-		} else {
-			document.querySelector("meta[name='description']").setAttribute('content','-')
-		}
-  	} 
-  },
-  mounted : function(){
-      var to = this.$route;
-      this.createPageTitle(to);
-  },
-  watch: { 
-      '$route' (to, from) {
-          this.createPageTitle(to);
-      }
-  }
-}
 </script>
