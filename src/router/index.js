95985735a1d0dd7784ff28ec7a9a5311771ff50c
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-03-29 10:51:44 +0900

    dd

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Login from "../views/Login.vue";
-import SignUp from "../views/SignUp.vue";
 
 Vue.use(VueRouter);
 
@@ -17,18 +16,12 @@ const routes = [
     path: "/about",
     name: "about",
     component: About,
-    meta: { title: 'イキツケSHARE　使い方',desc: '「お店を紹介したい人」と「新しくお店を開拓したい人」をつなげるwebサービスです。'}
   },
- {
-    path: "/signup",
-    name: "signup",
-    component: SignUp,
-  },
+ 
   {
     path: "/",
     name: "Home",
     component: Home,
-    meta: { title: 'イキツケSHARE', desc: '「お店を紹介したい人」と「新しくお店を開拓したい人」をつなげるwebサービスです。' }
   },
 ];
 
