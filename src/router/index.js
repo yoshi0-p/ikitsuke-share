8461920b2b5d5e0baa42737f5c5b38b0a38f12b6
Koji yoshida <koji@KojinoMacBook-Pro.local>
2021-04-21 13:23:33 +0900

    first-commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 import About from "../views/About.vue";
 import Login from "../views/Login.vue";
 import Top from "../views/Top.vue";
@@ -18,16 +17,11 @@ const routes = [
     name: "about",
     component: About,
   },
-  {
-    path: "/top",
-    name: "top",
-    component: Top,
-  },
- 
+  
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "top",
+    component: Top,
   },
 ];
 
